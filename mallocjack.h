cda3839b107089fe88f8c07517388539c1854e1b
Savio Machado <sav@loophole.cc>
2018-11-08 01:46:51 -0200

    mallocjack.h: fix style

--- mallocjack.h
+++ mallocjack.h
@@ -4,39 +4,40 @@
 #include <stdbool.h>
 #include <list.h>
 
-/* Filters are executed before each native call. If true is returned
- * then the native call is not called and the hook returns the corresponding
- * error if any.
+/**
+ * Filters are executed before each native call. If true is returned then the
+ * native call is not called and the hook returns error if not void.
  */
-struct mjack_filter {
-    bool (*malloc)   (size_t);
-    bool (*calloc)   (size_t, size_t);
-    bool (*realloc)  (void *, size_t);
-    bool (*memalign) (size_t, size_t);
-    bool (*free)     (void *);
+struct mjfilter {
+    bool (*malloc)(size_t);
+    bool (*calloc)(size_t, size_t);
+    bool (*realloc)(void *, size_t);
+    bool (*memalign)(size_t, size_t);
+    bool (*free)(void *);
 
     struct list list;
 };
 
-void mjack_filter_add(struct mjack_filter *);
+void mjfilter_add(struct mjfilter *);
 
-void mjack_filter_del(struct mjack_filter *);
+void mjfilter_del(struct mjfilter *);
 
-/* Traces are executed after the native call. The value returned
- * by the native call, if any, is passed as last argument.
+/**
+ * Traces are executed after the native call. The value returned by the native
+ * call, if any, is passed as last argument.
  */
-struct mjack_trace {
-    void (*malloc)   (size_t, void *);
-    void (*calloc)   (size_t, size_t, void *);
-    void (*realloc)  (void *, size_t, void *);
-    void (*memalign) (size_t, size_t, void *);
-    void (*free)     (void *);
+struct mjtrace {
+    void (*malloc)(size_t, void *);
+    void (*calloc)(size_t, size_t, void *);
+    void (*realloc)(void *, size_t, void *);
+    void (*memalign)(size_t, size_t, void *);
+    void (*free)(void *);
 
     struct list list;
 };
 
-void mjack_trace_add(struct mjack_trace *);
+void mjtrace_add(struct mjtrace *);
 
-void mjack_trace_del(struct mjack_trace *);
+void mjtrace_del(struct mjtrace *);
 
 #endif /* LIBMALLOCJACK_H */
